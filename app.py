3ea0643a1df5390d829169c461887c2c7e8d7c72
yamen <yamen225@gmail.com>
2020-04-03 17:04:40 +0200

    adjust settings for different environments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -13,4 +14,5 @@ def hello_name(name):
 
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
